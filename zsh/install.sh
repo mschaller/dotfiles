14537f986bd4cc07f4468e0f061a16b7edb2f3db
Michael Schaller <michael.schaller@swednet.de>
2019-08-03 20:57:22 +0200

    Add zsh-config with oh-my-zsh installer

---
+++ zsh/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+command_exists() {
+    command -v "$@" >/dev/null 2>&1
+}
+
+
+command_exists git || {
+    echo "can not install oh-my-zsh: git is not installed"
+    exit 1
+}
+
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.zshrc ~/.zshrc.orig
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
