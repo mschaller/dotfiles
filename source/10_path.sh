723eae985e6d14943ea2688589f093e597b71b70
Michael Schaller <michael.schaller@swednet.de>
2019-08-11 21:32:55 +0200

    Modify zsh to run source-scripts

--- source/10_path.sh
+++ source/10_path.sh
@@ -6,6 +6,15 @@ paths=(
 
 export PATH
 for p in "${paths[@]}"; do
-  [[ -d "$p" ]] && PATH="$p:$(path_remove "$p")"
-done
+    if [ -n "$ZSH_VERSION" ]; then
+        # just add; duplicates being removed in zshrc
+        PATH="$PATH:$p"
+    elif [ -n "$BASH_VERSION" ]; then
+        #[[ -d "$p" ]] && PATH="$p:$(path_remove/$p/ "$p")"
+        PATH="${PATH/p/}:$p"
+    else
+        PATH="$PATH:$p"
+    fi
+    done
 unset p paths
+
