d68dbcf250850dc2c03ccf798851f5799dfee48b
Michael Schaller <michael.schaller@swednet.de>
2021-03-06 13:35:01 +0100

    Modify initscreens script to use HDMI instead of DP

--- config/i3/initscreens.sh
+++ config/i3/initscreens.sh
@@ -4,8 +4,8 @@ host=$(hostname)
 if [ $host = "msi" ]
 then
         echo "setup msi screens"
-        xrandr --output HDMI-1-1 --auto --right-of DP-5
-        xrandr --output DP-5 --primary
+        xrandr --output HDMI-1-1 --auto --right-of HDMI-0
+        xrandr --output HDMI-0 --primary
         xrandr --output HDMI-1-1 --secondary
 else
         echo "no config available for hostname $host"
