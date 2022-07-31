637206cb785990c0a87c5a63d12b35296f6608be
Michael Schaller <michael.schaller@swednet.de>
2022-07-31 23:52:09 +0200

    feat(i3): align initscript to new setup

--- config/i3/initscreens.sh
+++ config/i3/initscreens.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-host=$(hostname)
+host=$(cat /etc/hostname)
 if [ $host = "msi" ]
 then
         echo "setup msi screens"
-        xrandr --output HDMI-1-1 --auto --right-of HDMI-0
-        xrandr --output HDMI-0 --primary
-        xrandr --output HDMI-1-1 --secondary
+        xrandr --output HDMI-0 --auto --primary 
+        xrandr --output DP-1 --auto --right-of HDMI-0
+#        xrandr --output HDMI-1 --primary
+#        xrandr --output DP-1 --secondary
 else
         echo "no config available for hostname $host"
 fi
